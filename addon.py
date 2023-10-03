a92a63751e69a4efe2fe3dd2e8df2cce30e4bae8
hwakeman <123672121+hwakeman@users.noreply.github.com>
2023-10-03 17:31:01 -0400

    Update addon.py

--- addon.py
+++ addon.py
@@ -5,6 +5,7 @@ current_config_file = open('UserData/current_config.json', "r+")
 profiles_data = json.load(profiles_file)
 current_config_data = json.load(current_config_file)
 
+#Adds a profile to profiles.json
 def add_profile(profile_dict):
     global profiles_data, profiles_file
     profiles_data.append(profile_dict)
@@ -13,6 +14,7 @@ def add_profile(profile_dict):
     profiles_file = open('UserData/profiles.json', "r+")
     profiles_data = json.load(profiles_file)
 
+#Deletes a profile from profiles.json
 def delete_profile(config_name):
     global profiles_data, profiles_file
     for profile in profiles_data:
@@ -23,6 +25,7 @@ def delete_profile(config_name):
     profiles_file = open('UserData/profiles.json', "r+")
     profiles_data = json.load(profiles_file)
 
+#Updates the current config being used and writes it to current_config.json
 def update_current_config(config_name):
     global current_config_data, current_config_file
     config_to_update = {}
