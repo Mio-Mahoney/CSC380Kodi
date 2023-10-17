68e9885f21573235b715efef1b08b9e28af183fd
hwakeman <123672121+hwakeman@users.noreply.github.com>
2023-10-16 20:16:29 -0400

    Updated addon.py with more more fleshed out UI

--- addon.py
+++ addon.py
@@ -2,37 +2,110 @@ import xbmc
 import xbmcaddon
 import xbmcgui
 
+from src import json_utils
+
 addon = xbmcaddon.Addon()
 addonname = addon.getAddonInfo('name')
 dialog = xbmcgui.Dialog()
 window = xbmcgui.Window()
 
+# User configuration data that will be used throughout the program
+genre_choice = []
+watched_unwatched_choice = []
+length_choice = []
+rating_choice = []
+
+# Displays main page where the user can choose to randomly select, configure, or choose profile
 def display_random_button():
-    user_choice = dialog.yesnocustom("Kodi Episode Picker", "What would you like to do?", "", "Configuration", "Random Select")
-    if user_choice:
-        dialog.ok("Kodi Episode Picker","This is where our randomization will occur")
-    else:
+    # user_choice is assigned based on the user's choice. 
+    # 0 : Configuration
+    # 1 : Random Select
+    # 2 : Choose Profile
+    user_choice = dialog.yesnocustom("Kodi Episode Picker", "What would you like to do?", "Choose Profile", "Configuration", "Random Select")
+    if user_choice == 0:
         display_configuration_page()
+    elif user_choice == 1:
+        json_utils.read_current_configuration()
+        user_choice = dialog.yesno("Kodi Episode Picker", "Would you like to choose media based on your mood today?")
+        if user_choice:
+            display_mood_page()
+        else:
+            play_random_media()
+    else:
+        display_profile_page()
+        
+
+# Displays a page where the user can choose what profile they want to use
+def display_profile_page():
+    profile_names = json_utils.get_profile_names()
+    profile_labels = [f"{profile}" for profile in profile_names]
+    profile_choice = dialog.select("Select Profile you want to use", profile_labels)
+    dialog.ok("Kodi Episode Picker", f"Your random play button is now configured to {profile_labels[profile_choice]}!")
+
 
 def display_configuration_page():
     # Arrays that hold possible choices for the different configurations
-    genres = ["Any","Action", "Comedy", "Drama", "Horror", "Science Fiction"]
-    lengths = ["Any", "0-30 Minutes", "31-60 minutes", "61-90 minutes","91-120 minutes","121-180 minutes",">180 minutes",]
+    genres = ["Action", "Comedy", "Drama", "Horror", "Science Fiction"]
+    lengths = ["0-30 Minutes", "31-60 minutes", "61-90 minutes","91-120 minutes","121-180 minutes",">180 minutes",]
+    ratings = ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", "9-10"]
 
     genre_labels = [f"{genre}" for genre in genres]
     length_labels = [f"{length}" for length in lengths]
-     
-    # Create a multiselect list for genres
-    genre_choice = dialog.multiselect("Select Movie Genres", genre_labels)
-    
-    # Create a yes/no control for watched/unwatched
-    watched_unwatched_choice = dialog.yesnocustom("Kodi Episode Picker", "Do you want to see media you've already watched?", "Unwatched", "Watched", "Any")
-    
-    # Create a select control for movie length
-    length_choice = dialog.multiselect("Select Movie Length", length_labels)
+    rating_labels = [f"{rating}" for rating in ratings]
 
+    # Controls for user to input configurations
+    watched_unwatched_choice = dialog.yesnocustom("Kodi Episode Picker", "Do you want to see media you've already watched?", "Unwatched", "Watched", "Any")
+    genre_choice_nums = dialog.multiselect("Select Movie Genres", genre_labels)
+    genre_choice = convert_indexes_to_strings(genre_choice_nums, genre_labels)
+    length_choice_nums = dialog.multiselect("Select Movie Length", length_labels)
+    length_choice = convert_indexes_to_strings(length_choice_nums, length_labels)
+    rating_choice_nums = dialog.multiselect("Select Movie Rating", rating_labels)
+    rating_choice = convert_indexes_to_strings(rating_choice_nums, rating_labels)
+
+    # Controls for user to save configuration as profile
+    save_as_profile = dialog.yesno("Kodi Episode Picker", "Would you like to save this configuration as a profile?")
+    if(save_as_profile):
+        profile_name = dialog.input("What do you want the name of the profile to be?")
+        profile_dict = create_profile_dict()
+        json_utils.add_profile(profile_dict)
+    else:
+        dialog.ok("Kodi Episode Picker","Your configuration is now ready to play random media!")
+
+# This function takes the indices that the multiselect controls produce and fills 
+# them with the corresponding strings. This will make it much easier to program with
+# (Example: the index 0 will instead be "Science Fiction" in genre_choice variable)
+def convert_indexes_to_strings(int_arr, string_arr):
+    result_arr = []
+    for index in int_arr:
+        result_arr.append(string_arr[index])
+
+
+
+
+
+# -----------------------------------------------------------------------------------------------------
+# Unfinished Functions
+# -----------------------------------------------------------------------------------------------------
+
+# This function will display the mood page
+def display_mood_page():
+    dialog.ok("Kodi Episode Picker","This is where our mood page will go.")
+
+# This function will use the user configuration data arrays to randomly choose from media
+def play_random_media():
+    media_location = "C:/KodiMovies/Lady_Frankenstein.mp4"
+    xbmc.Player().play(media_location)
+
+def create_profile_dict():
+    pass
+
+# -----------------------------------------------------------------------------------------------------
+# Starts Program
+# -----------------------------------------------------------------------------------------------------
 
 display_random_button()
+
+
 
 
 
