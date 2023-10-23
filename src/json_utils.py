38dc28b653acb0f6fb919e0643102923f7d84b04
hwakeman <123672121+hwakeman@users.noreply.github.com>
2023-10-23 16:18:18 -0400

    Update json_utils.py

--- src/json_utils.py
+++ src/json_utils.py
@@ -2,42 +2,42 @@ import json
 import xbmcvfs
 
 root_path = xbmcvfs.translatePath("special://xbmc/addons/script.episode.picker") + "\\UserData\\"
-profiles_file_path = root_path+'profiles.json'
+presets_file_path = root_path+'presets.json'
 current_config_file_path = root_path+'current_config.json'
 
-profiles_file = open(profiles_file_path, "r+")
+presets_file = open(presets_file_path, "r+")
 current_config_file = open(current_config_file_path, "r+")
-profiles_data = json.load(profiles_file)
+presets_data = json.load(presets_file)
 current_config_data = json.load(current_config_file)
 
-#Adds a profile to profiles.json
-def add_profile(profile_dict):
-    global profiles_data, profiles_file
-    profiles_data.append(profile_dict)
-    with open(profiles_file_path, 'w') as json_file:
-            json.dump(profiles_data, json_file, indent=4)
-    profiles_file = open(profiles_file_path, "r+")
-    profiles_data = json.load(profiles_file)
+#Adds a preset to presets.json
+def add_preset(preset_dict):
+    global presets_data, presets_file
+    presets_data.append(preset_dict)
+    with open(presets_file_path, 'w') as json_file:
+            json.dump(presets_data, json_file, indent=4)
+    presets_file = open(presets_file_path, "r+")
+    presets_data = json.load(presets_file)
 
-#Deletes a profile from profiles.json
-def delete_profile(config_name):
-    global profiles_data, profiles_file
-    for profile in profiles_data:
-        if profile.get("name") == config_name:
-            profiles_data.remove(profile)
-    with open(profiles_file_path, 'w') as json_file:
-            json.dump(profiles_data, json_file, indent=4)
-    profiles_file = open(profiles_file_path, "r+")
-    profiles_data = json.load(profiles_file)
+#Deletes a preset from presets.json
+def delete_preset(config_name):
+    global presets_data, presets_file
+    for preset in presets_data:
+        if preset.get("name") == config_name:
+            presets_data.remove(preset)
+    with open(presets_file_path, 'w') as json_file:
+            json.dump(presets_data, json_file, indent=4)
+    presets_file = open(presets_file_path, "r+")
+    presets_data = json.load(presets_file)
 
 #Updates the current config being used and writes it to current_config.json
 def update_current_config(config_name):
     global current_config_data, current_config_file
     config_to_update = {}
     name_exists = False
-    for profile in profiles_data:
-        if profile.get("name") == config_name:
-            config_to_update = profile
+    for preset in presets_data:
+        if preset.get("name") == config_name:
+            config_to_update = preset
             name_exists = True
             break
     if name_exists:
@@ -48,11 +48,11 @@ def update_current_config(config_name):
     current_config_file = open(current_config_file_path, "r+")
     current_config_data = json.load(current_config_file)
 
-# This function will obtain the names of all of the user profiles from our UserData json files
-def get_profile_names():
-    global profiles_data
+# This function will obtain the names of all of the user presets from our UserData json files
+def get_preset_names():
+    global presets_data
     names = []
-    for item in profiles_data:
+    for item in presets_data:
         names.append(item["name"])
     return names
 
@@ -64,6 +64,6 @@ def get_profile_names():
 # Unfinished Functions
 # -----------------------------------------------------------------------------------------------------
 
-# This function will save a user's profile to our UserData json files
-def add_profile(profile_name):
+# This function will save a user's preset to our UserData json files
+def add_preset(preset_name):
     pass
