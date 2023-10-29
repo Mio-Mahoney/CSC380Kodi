1256828fb4f83bd4f0b5e988c24d25151f02a8c1
Hwakeman <hwakema2@oswego.edu>
2023-10-29 00:32:24 -0400

    Added add, remove, and edit features for profiles and cleared json files

--- addon.py
+++ addon.py
@@ -18,35 +18,64 @@ release_year_choice = []
 ratings_choice = []
 languages_choice = []
 parental_advisory_choice = []
-closed_captions_choice = -1
+closed_captions_choice = False
 
 # Displays main page where the user can choose to randomly select, configure, or choose preset
 def display_home_page():
     # user_choice is assigned based on the user's choice. 
-    # 0 : Configuration
+    # 0 : Presets
     # 1 : Random Select
-    # 2 : Choose Preset
-    user_choice = dialog.yesnocustom("Kodi Episode Picker", "What would you like to do?", "Choose Preset", "Configuration", "Random Select")
+    # 2 : Search Across Streaming Services
+    user_choice = dialog.yesnocustom("Kodi Episode Picker", "What would you like to do?", "Search Services", "Presets", "Random Select")
     if user_choice == 0:
-        display_configuration_page()
+        display_preset_page()
     elif user_choice == 1:
-        config = json_utils.current_config_data
         user_choice = dialog.yesno("Kodi Episode Picker", "Would you like to choose media based on your mood today?")
         if user_choice:
             display_mood_page()
         else:
             play_random_media()
     else:
-        display_preset_page()
+        display_streaming_service_page()
 
 # Displays a page where the user can choose what preset they want to use
 def display_preset_page():
     preset_names = json_utils.get_preset_names()
+    preset_labels_without_options = [f"{preset}" for preset in preset_names]
+    preset_labels = preset_labels_without_options
+    preset_labels.insert(0, "Edit Preset")
+    preset_labels.insert(0, "Delete Preset")
+    preset_labels.insert(0, "Add Preset")
+    preset_choice_num = dialog.select("Choose Preset", preset_labels)
+    if preset_choice_num == 0:
+        display_configuration_page()
+    elif preset_choice_num == 1: 
+        display_preset_delete_page()
+    elif preset_choice_num == 2: 
+        display_preset_edit_page()
+    else:
+        preset_choice = preset_labels[preset_choice_num]
+        json_utils.update_current_config(preset_choice)
+        dialog.ok("Kodi Episode Picker", f"Your random play button is now configured to {preset_choice}!")
+
+def display_preset_delete_page():
+    preset_names = json_utils.get_preset_names()
     preset_labels = [f"{preset}" for preset in preset_names]
-    preset_choice_num = [dialog.select("Select preset you want to use", preset_labels)]
+    preset_choice_num = [dialog.select("Choose what preset to delete", preset_labels)]
     preset_choice = convert_indexes_to_strings(preset_choice_num, preset_labels)[0]
-    json_utils.update_current_config(preset_choice)
-    dialog.ok("Kodi Episode Picker", f"Your random play button is now configured to {preset_choice}!")
+    json_utils.delete_preset(preset_choice)
+    if preset_choice == json_utils.get_current_config()["name"]:
+        json_utils.delete_current_config()
+    dialog.ok("Kodi Episode Picker", f"You have deleted the preset {preset_choice}!")
+
+def display_preset_edit_page():
+    preset_names = json_utils.get_preset_names()
+    preset_labels = [f"{preset}" for preset in preset_names]
+    preset_choice_num = [dialog.select("Choose what preset to edit", preset_labels)]
+    preset_choice = convert_indexes_to_strings(preset_choice_num, preset_labels)[0]
+    preset_dict = json_utils.get_preset(preset_choice)
+    load_preset(preset_dict)
+    display_configuration_page()
 
 # Displays the configuration home page       
 def display_configuration_page():
@@ -83,6 +112,9 @@ def display_configuration_page():
         display_preset_exit_page()
         display_home_page()
 
+def display_streaming_service_page():
+    media_name = dialog.input("What media do you want to find today?")
+
 def display_mood_page():
     moods = ["Happy", "Sad", "Scary", "Surprising", "Relaxing", "Uplifting"]
     mood_labels = [f"{mood}" for mood in moods]
@@ -91,10 +123,12 @@ def display_mood_page():
 
 # Displays a page where the user can choose if they want to see media they've already seen
 def display_watch_status_page():
+    global watch_status_choice
     watch_status_choice = dialog.yesnocustom("Kodi Episode Picker", "Do you want to see media you've already watched?", "Unwatched", "Any", "Watched")
 
 # Displays a page where the user can choose what genres they want to watch
 def display_genre_page():
+    global genre_choice
     genres = ["Action", "Comedy", "Drama", "Horror", "Science Fiction"]
     genre_labels = [f"{genre}" for genre in genres]
     genre_choice_nums = dialog.multiselect("Select Movie Genres", genre_labels)
@@ -102,6 +136,7 @@ def display_genre_page():
 
 # Displays a page where the user can choose what lengths of media they want to watch
 def display_length_page():
+    global length_choice
     lengths = ["0-30 Minutes", "31-60 minutes", "61-90 minutes","91-120 minutes","121-180 minutes",">180 minutes"]
     length_labels = [f"{length}" for length in lengths]
     length_choice_nums = dialog.multiselect("Select Movie Length", length_labels)
@@ -109,27 +144,36 @@ def display_length_page():
 
 # Displays a page where the user can choose what release years they would like to watch
 def display_release_year_page():
-    years = [str(year) for year in range(1900, 2031)][::-1]
-    release_year_choice.append(dialog.select("Choose Minimum Year", years))
-    release_year_choice.append(dialog.select("Choose Maximum Year", years))
+    global release_year_choice
+    min_year = 1900
+    max_year = 2030
+    years = [str(year) for year in range(min_year, max_year + 1)][::-1]
+    min_year_input = max_year - dialog.select("Choose Minimum Year", years)
+    max_year_input = max_year - dialog.select("Choose Maximum Year", years)
+    release_year_choice = []
+    release_year_choice.append(min_year_input)
+    release_year_choice.append(max_year_input)
 
 
 # Displays a page where the user can choose what ratings they would like to watch
 def display_ratings_page():
+    global ratings_choice
     ratings = ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", "9-10"]
-    rating_labels = [f"{rating}" for rating in ratings]
-    rating_choice_nums = dialog.multiselect("Select Movie Ratings", rating_labels)
-    rating_choice = convert_indexes_to_strings(rating_choice_nums, rating_labels)
+    ratings_labels = [f"{rating}" for rating in ratings]
+    ratings_choice_nums = dialog.multiselect("Select Movie Ratings", ratings_labels)
+    ratings_choice = convert_indexes_to_strings(ratings_choice_nums, ratings_labels)
 
 # Displays a page where the user can choose what languages they would like to watch
 def display_languages_page():
+    global languages_choice
     languages = ["English", "Spanish", "German", "French", "Mandarin", "Hindi", "Arabic", "Portuguese", "Bengali", "Russian", "Japanese"]
     language_labels = [f"{language}" for language in languages]
     language_choice_nums = dialog.multiselect("Select Movie Languages", language_labels)
-    languages_choice_choice = convert_indexes_to_strings(language_choice_nums, language_labels)
+    languages_choice = convert_indexes_to_strings(language_choice_nums, language_labels)
 
 # Displays a page where the user can choose what parental advisory ratings they would like to watch
 def display_parental_advisory_page():
+    global parental_advisory_choice
     parent_ratings = ["G", "PG", "PG-13", "R", "NR"]
     parent_rating_labels = [f"{parent_rating}" for parent_rating in parent_ratings]
     parent_rating_choice_nums = dialog.multiselect("Select Parental Advisory Ratings", parent_rating_labels)
@@ -137,15 +181,25 @@ def display_parental_advisory_page():
 
 # Displays a page where the user can choose if they want to only watch media with closed captions
 def display_closed_captions_page():
+    global closed_captions_choice
     closed_captions_choice = dialog.yesno("Kodi Episode Picker", "Do you want to only watch media with closed captions?")
 
 # Controls for user to save configuration as preset
 def display_preset_save_page():
-    preset_name = dialog.input("What do you want the name of the preset to be?")
-    preset_dict = create_preset_dict()
-    json_utils.add_preset(preset_dict)
-    reset_config()
-    dialog.ok("Kodi Episode Picker","Your configuration is now ready to play random media!")
+    global preset_name
+    if preset_name == "":
+        preset_name = dialog.input("What do you want the name of the preset to be?")
+        preset_dict = create_preset_dict()
+        json_utils.add_preset(preset_dict)
+        json_utils.update_current_config(preset_name)
+        reset_config()
+        dialog.ok("Kodi Episode Picker","Your preset is now ready to play random media!")
+    else:
+        preset_dict = create_preset_dict()
+        json_utils.replace_preset(preset_dict)
+        json_utils.update_current_config(preset_name)
+        reset_config()
+        dialog.ok("Kodi Episode Picker","Your preset is now ready to play random media!")
 
 def display_preset_exit_page():
     user_choice = dialog.yesno("Kodi Episode Picker", "Do you want to stop configuring this preset? Your current configurations won't be saved.")
@@ -177,6 +231,19 @@ def reset_config():
     languages_choice = []
     parental_advisory_choice = []
     closed_captions_choice = -1
+
+def load_preset(preset_dict):
+    global preset_name, watch_status_choice, genre_choice, length_choice, release_year_choice, ratings_choice, languages_choice, parental_advisory_choice, closed_captions_choice
+    preset_name = preset_dict["name"]
+    watch_status_choice = preset_dict["watch-status"]
+    genre_choice = preset_dict["genres"]
+    length_choice = preset_dict["media-length"]
+    release_year_choice = preset_dict["release-year"]
+    ratings_choice = preset_dict["ratings"]
+    languages_choice = preset_dict["languages"]
+    parental_advisory_choice = preset_dict["parental-advisory"]
+    closed_captions_choice = preset_dict["closed-captions"]
+    
 
 def create_preset_dict():
     return {
--- src/json_utils.py
+++ src/json_utils.py
@@ -5,19 +5,39 @@ root_path = xbmcvfs.translatePath("special://home/addons/script.episode.picker/U
 presets_file_path = root_path+'presets.json'
 current_config_file_path = root_path+'current_config.json'
 
-presets_file = open(presets_file_path, "r+")
-current_config_file = open(current_config_file_path, "r+")
-presets_data = json.load(presets_file)
-current_config_data = json.load(current_config_file)
+presets_file = ""
+current_config_file = ""
+presets_data = ""
+current_config_data = ""
+
+try:
+    presets_file = open(presets_file_path, "r+")
+    current_config_file = open(current_config_file_path, "r+")
+    presets_data = json.load(presets_file)
+    current_config_data = json.load(current_config_file)
+        
+except (FileNotFoundError, json.decoder.JSONDecodeError):
+    pass
+
+
+# This function will get a preset with the given name
+def get_preset(preset_name):
+    global presets_data, presets_file
+    for item in presets_data:
+        if item["name"] == preset_name:
+            return item
+    return None
 
 #Adds a preset to presets.json
 def add_preset(preset_dict):
-    global presets_data, presets_file
+    try:
+        with open(presets_file_path, 'r') as json_file:
+            presets_data = json.load(json_file)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        presets_data = []
     presets_data.append(preset_dict)
     with open(presets_file_path, 'w') as json_file:
-            json.dump(presets_data, json_file, indent=4)
-    presets_file = open(presets_file_path, "r+")
-    presets_data = json.load(presets_file)
+        json.dump(presets_data, json_file, indent=4)
 
 #Deletes a preset from presets.json
 def delete_preset(config_name):
@@ -29,6 +49,16 @@ def delete_preset(config_name):
             json.dump(presets_data, json_file, indent=4)
     presets_file = open(presets_file_path, "r+")
     presets_data = json.load(presets_file)
+
+def replace_preset(preset_dict):
+    global presets_data, presets_file
+    delete_preset(preset_dict["name"])
+    add_preset(preset_dict)
+
+#Gets the current config being used
+def get_current_config():
+    global current_config_data
+    return current_config_data
 
 #Updates the current config being used and writes it to current_config.json
 def update_current_config(config_name):
@@ -48,6 +78,13 @@ def update_current_config(config_name):
     current_config_file = open(current_config_file_path, "r+")
     current_config_data = json.load(current_config_file)
 
+#Deletes the current config in current_config.json
+def delete_current_config():
+    global current_config_data, current_config_file
+    config_to_update = {}
+    with open(current_config_file_path, 'w') as json_file:
+            json.dump(config_to_update, json_file, indent=4)
+
 # This function will obtain the names of all of the user presets from our UserData json files
 def get_preset_names():
     global presets_data
@@ -55,15 +92,3 @@ def get_preset_names():
     for item in presets_data:
         names.append(item["name"])
     return names
-
-
-
-
-
-# -----------------------------------------------------------------------------------------------------
-# Unfinished Functions
-# -----------------------------------------------------------------------------------------------------
-
-# This function will save a user's preset to our UserData json files
-def add_preset(preset_name):
-    pass
